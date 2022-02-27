b0f1fc6da6be515e33f1ec646bc79ad95bf64e51
123zmz123 <1245539137@qq.com>
2022-02-27 17:37:28 +0800

    modify zaux
    add basic functions in zaux, seems like we can test it and debug for good understanding

--- zaux.c
+++ zaux.c
@@ -1,4 +1,3 @@
-
 #include "zaux.h"
 #include "zdo.h"
 
@@ -6,7 +5,8 @@ static void* z_alloc(void* ud, void* ptr, size_t osize, size_t nsize) {
     (void)ud;
     (void)osize;
 
-    if(nsize == 0) {
+
+    if (nsize == 0) {
         free(ptr);
         return NULL;
     }
@@ -39,6 +39,75 @@ void zuaZ_pushnil(struct zua_State* Z) {
     zua_pushnil(Z);
 }
 
-void zuaZ_pushfunction(struct zua_State* Z, zua_CFunction f) {
-    
+void zuaZ_pushcfunction(struct zua_State* Z, zua_CFunction f) {
+    zua_pushcfunction(Z, f);
+}
+
+void zuaZ_pushboolean(struct zua_State* Z, bool boolean) {
+    zua_pushboolean(Z, boolean);
+}
+
+
+typedef struct CallS {
+    StkId func;
+    int nresult;
+} CallS;
+
+static int f_call(zua_State* Z, void* ud) {
+    CallS* c = cast(CallS*, ud);
+    zuaD_call(Z, c->func, c->nresult);
+    return ZUA_OK;
+}
+
+int zuaZ_pcall(struct zua_State* Z, int narg, int nresult) {
+    int status = ZUA_OK;
+    CallS c;
+    c.func = Z->top - (narg + 1); // func were reside below the input patameter
+    c.nresult = nresult;
+    // savestack = Z->top - Z->stack
+    status = zuaD_pcall(Z, &f_call, &c, savestack(Z, Z->top), 0); 
+    return status;
+}
+
+bool zuaZ_checkinteger(struct zua_State* Z, int idx) {
+    int isnum = 0;
+    zua_tointegerx(Z, idx, &isnum); // try to convert it to integer
+    if (isnum) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+zua_Integer zuaZ_tointeger(struct zua_State* Z, int idx) {
+    int isnum = 0;
+    zua_Integer ret = zua_tointegerx(Z, idx, &isnum);
+    return ret;
+}
+
+zua_Number zuaZ_tonumber(struct zua_State* Z, int idx) {
+    int isnum = 0;
+    zua_Number ret = zua_tonumberx(Z, idx, &isnum);
+    return ret;
+}
+
+void *zuaZ_touserdata(struct zua_State* Z, int idx) {
+    return NULL;
+}
+
+bool zuaZ_toboolean(struct zua_State *Z, int idx) {
+    return zua_toboolean(Z, idx);
+}
+
+int zuaZ_isnil(struct zua_State* Z, int idx) {
+    return zua_isnil(Z, idx);
+}
+
+void zuaZ_pop(struct zua_State* Z) {
+    zua_pop(Z);
+}
+
+int zuaZ_stacksize(struct zua_State* Z) { // how many free space do we have 
+    return zua_gettop(Z);
 }
--- zaux.h
+++ zaux.h
@@ -1,5 +1,27 @@
 #ifndef _ZAUX_H
 #define _ZUAX_H
+
 #include "zstate.h"
+
+struct zua_State* zuaL_newstate();
+void zuaZ_close(struct zua_State* Z);
+
+void zuaZ_pushinteger(struct zua_State* Z, int integer);
+void zuaZ_pushnumber(struct zua_State* Z, float number);
+void zuaZ_pushlightuserdata(struct zua_State* Z, void* userdata);
+void zuaZ_pushnil(struct zua_State* Z);
+void zuaZ_pushcfunction(struct zua_State* Z, zua_CFunction f);
+void zuaZ_pushboolean(struct zua_State* Z, bool boolean);
+int zuaZ_pcall(struct zua_State* Z, int narg, int nresult);
+
+bool zuaZ_checkinteger(struct zua_State* Z, int idx);
+zua_Integer zuaZ_tointeger(struct zua_State* Z, int idx);
+zua_Number zuaZ_tonumber(struct zua_State* Z, int idx);
+void* zuaZ_touserdata(struct zua_State* Z, int idx);
+bool zuaZ_toboolean(struct zua_State* Z, int idx);
+int zuaZ_isnil(struct zua_State* Z, int idx);
+
+void zuaZ_pop(struct zua_State* Z);
+int zuaZ_stacksize(struct zua_State* Z);
 
 #endif // !ZAUX_H
